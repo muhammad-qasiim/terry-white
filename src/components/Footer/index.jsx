f71165f279c88462db5be6050ff132ae27bcdee3
pixelswd46 <pixelswd46@outlook.com>
2022-05-30 11:02:44 +0500

    navbar updated

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
     return (
         <>
             <footer>
-                <section className="bg-footer">
+                <section className="bg-gray-100">
                     <section className="container mx-auto px-24 lg:px-99 py-24 text-center">
-                        <span className="text-gray-400 text-14"> Copyright © 2022 ConcertRx Pty Ltd.</span>
+                        <span className="text-gray-800 text-14"> Copyright © 2022 Concert<span className="text-primary">Rx</span>  Pty Ltd.</span>
                     </section>
                 </section>
             </footer>
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,54 +1,6 @@
-
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom'
-import { Link } from 'react-router-dom';
-import { AiOutlineMenu } from "react-icons/ai";
-import { VscClose } from "react-icons/vsc";
-import Sidebar from '../Sidebar';
-
-
 const Navbar = () => {
-    const location = useLocation();
-
-    const [toggleSidebar, setToggleSidebar] = useState(false);
-    const Navigation = [{ placeholder: 'Home', path: '/' }, { placeholder: 'Covid-19 Vaccine', path: '/covid-vaccine' }, { placeholder: 'FLU Vaccine', path: '/flu-vaccine' }, { placeholder: 'Free RATs', path: '/free-rats' }, { placeholder: 'Locations', path: '/locations' }]
-
     return (
-        <>
-
-            <nav className="h-71">
-                <main className="flex h-71 justify-between items-center mx-auto px-24 lg:px-99">
-                    <Link to="/">
-                        <h3 className='font-semibold text-20'>ConcertRx</h3>
-                    </Link>
-
-                    <button onClick={() => setToggleSidebar(!toggleSidebar)} type="button" className="relative inline-flex items-center p-2 ml-14 text-sm text-gray-500 rounded-xl xl:hidden  focus:outline-none focus:text-gray-800" aria-controls="mobile-menu-3" aria-expanded="false">
-                        {toggleSidebar ? <VscClose size="24" /> : <AiOutlineMenu size="20" />}
-                    </button>
-
-                    <div className="hidden justify-between items-center w-full xl:flex xl:w-auto ">
-                        <ul className="flex flex-col items-center mt-4 xl:flex-row xl:space-x-8 xl:mt-0 xl:text-sm xl:font-medium">
-                            {
-                                Navigation?.map(item => (
-                                    <li key={Math.random()}>
-                                        <Link to={item?.path}>
-                                            <span className={`text-lg cursor-pointer transition-all hover:text-primary ${item?.path === location?.pathname ? 'text-primary' : 'text-gray-900'}`}>
-                                                {item?.placeholder}
-                                            </span>
-                                        </Link>
-                                    </li>
-                                ))
-                            }
-
-                        </ul>
-                    </div>
-
-                    <Sidebar toggleSidebar={toggleSidebar} setToggleSidebar={setToggleSidebar} />
-
-                </main>
-            </nav>
-        </>
-
+        <h3 className="text-22 font-semibold text-gray-800 py-22 text-center">Concert<span className="text-primary">Rx</span></h3>
     )
 }
 
