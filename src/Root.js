fbb350b3cb32ecc739b0d5d031f68157ebc9812a
qasim16 <qasim@sociabletech.com.au>
2022-05-31 03:14:49 +0500

    typo

--- src/Root.js
+++ src/Root.js
@@ -7,10 +7,6 @@ function App() {
     <>
       <Navbar />
 
-      <section className="container mx-auto px-24 lg:px-99 my-42 lg:my-99">
-         <p className="mt-4 text-18 text-gray-700 text-center px-0 lg:px-99">ConcertRx is a communication tool that sends the customer a programmed static or dynamic SMS messages when a patient card is dragged across a kanban board.</p>
-      </section>
-
       <YoutubeVideo />
 
       <Footer />
